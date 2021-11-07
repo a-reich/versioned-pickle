19c2d41d9882a9191526e7ce40a3cf441459135e
Asaf Reich <asafspades@gmail.com>
2021-11-06 21:25:55 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import setuptools
+
+# read README file
+from os import path
+ROOT_DIR = Path(__file__).parent.resolve()
+
+with open(ROOT_DIR / 'README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="versioned-pickle", # Replace with your own username
+    # SCM stuff is added here as well, for compatibility with tools that don't yet use the info
+    # from pyproject.toml (e.g. setup.py commands)
+    use_scm_version={'local_scheme': 'dirty-tag'},
+    setup_requires=['setuptools_scm'],
+    packages=setuptools.find_packages(),
+    author="Asaf Reich",
+    author_email="asafspades@gmail.com",
+    description="A small utility package for adding environment metadata to pickles and warning on mismatch when loaded",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires = ['importlib-metadata>=4.4;python<3.10'],
+    url="https://github.com/a-reich/vesrioned_pickle",
+    license='MIT License',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
---
+++ versioned_pickle/__init__.py
@@ -0,0 +1,39 @@
+"""Top level module for peopyl_density package."""
+
+import io
+import pickle
+from dataclasses import dataclass
+
+try:
+    from importlib.metadata import packages_distributions
+except ImportError, ModuleNotFoundError:
+    # needed function was added to the stdlib module in python 3.10, otherwise fall back to 3rd-party version
+    from importlib_metadata import packages_distributions
+
+
+class _VersionedPickler(pickle.Pickler):
+    def __init__(self, package_scope='object', *args, **kwargs):
+        self.package_scope = package_scope
+        self.module_names_found = set()
+        setup().__init__(*args, **kwargs)
+    def reducer_override(self, obj):
+        """Custom reducer that wraps the normal pickle operation, recording the modules defining
+        the type of each traversed object in the hierarchy. (Note: support for reducer_override was added to pickle in 3.8).
+        Modules are stored as a set of strings in self.module_names_found."""
+        print("found type:", type(obj), type(obj).__module__)
+        self.module_names_found.append(type(obj).__module__)
+        # continue back to usual reduction
+        return NotImplemented
+
+
+def dump(obj, package_scope='object', *args, **kwargs):
+    pickler = _VersionedPickler(package_scope, *args, **kwargs)
+    pickler.dump(obj)
+
+
+# @dataclass
+# class MyCls:
+# 	x:...
+#     y:... = None
+#
+# inst = MyCls(1)
