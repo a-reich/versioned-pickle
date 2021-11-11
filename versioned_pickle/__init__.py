f531f2233f8291c3613473663551666db8b06a2d
Asaf Reich <asafspades@gmail.com>
2021-11-11 17:22:39 -0500

    add metadata class, convert module names to dist versions

--- versioned_pickle/__init__.py
+++ versioned_pickle/__init__.py
@@ -1,15 +1,44 @@
 """Main module."""
+from __future__ import annotations
 
 import io
 import pickle
+import sys
 from dataclasses import dataclass
 
 try:
-    from importlib.metadata import packages_distributions
+    from importlib.metadata import packages_distributions, version
 except ImportError:
     # needed function was added to the stdlib module in python 3.10, otherwise fall back to 3rd-party version
-    from importlib_metadata import packages_distributions
+    from importlib_metadata import packages_distributions, version
 
+@dataclass(init=False)
+class EnvironmentMetadata:
+    packages: ...
+    py_ver: ...
+
+    def __init__(self, packages, py_ver):
+        self.packages = packages # TODO make dists
+        self.py_ver = py_ver
+    def to_dict(self):
+        result = {'environment_metadata':
+            {'packages': self.packages, 'py_ver': self.py_ver}}
+        return result
+    @classmethod
+    def from_dict(cls, metadata):
+        contents = metadata['environment_metadata']
+        return cls(contents['packages'], contents['py_ver'])
+    @classmethod
+    def from_modules(cls, module_names):
+        packages = _get_distributions_from_modules(module_names)
+        pkg_versions = {pkg: version(pkg) for pkg in packages}
+        return cls(pkg_versions, sys.version_info[:3])
+
+def _get_distributions_from_modules(module_names):
+    toplevel_pkgs = {mod.split('.')[0] for mod in module_names}
+    pkg_to_dists_dict = packages_distributions()
+    dists = {dist for pkg in toplevel_pkgs for dist in pkg_to_dists_dict.get(pkg, {})}
+    return dists
 
 class _VersionedPickler(pickle.Pickler):
     def __init__(self, file, *args, package_scope='object', **kwargs):
@@ -31,17 +60,26 @@ class _VersionedPickler(pickle.Pickler):
         f = io.BytesIO()
         super().dump(obj)
 
-
 def dump(obj, file, package_scope='object'):
     f_temp = io.BytesIO()
     pickler = _VersionedPickler(f_temp, package_scope=package_scope)
     pickler.dump(obj)
     pickled_obj = f_temp.getvalue()
     f_temp.close()
-    pickle.dump(pickler.module_names_found, file)
+    meta_info = EnvironmentMetadata.from_modules(pickler.module_names_found)
+    pickle.dump(meta_info.to_dict(), file)
     file.write(pickled_obj)
+
+def load(file, return_meta=False):
+    header_dict = pickle.load(file)
+    meta = EnvironmentMetadata.from_dict(header_dict)
+    val = pickle.load(file)
+    return (val, meta) if return_meta else val
 
 def dumps(obj, package_scope='object'):
     f = io.BytesIO()
     dump(obj, f)
     return f.getvalue()
+def loads(f, return_meta=False):
+    obj = load(obj, f)
+    return obj
